1e32810481b129b37ae5ba00ffedefdd828bf91e
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2010-02-11 07:21:46 +0100

    Fix ref to juglr-0.2.0.jar in higgla.sh that should have been juglr-0.2.1.jar and make the script exec java instead of spawning java as a child process

--- higgla.sh
+++ higgla.sh
@@ -1,1 +1,1 @@
-java -ea -Xbootclasspath/p:lib/jsr166.jar -cp lib/lucene-core-3.0.0.jar:lib/juglr-0.2.0.jar:higgla-0.0.1.jar higgla.server.Main
+exec java -ea -Xbootclasspath/p:lib/jsr166.jar -cp lib/lucene-core-3.0.0.jar:lib/juglr-0.2.1.jar:higgla-0.0.1.jar higgla.server.Main
