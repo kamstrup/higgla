075c3483c478577def40dfde456f7825dada2fa2
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2010-02-16 15:06:35 +0100

    WIP : start porting to Juglr's trunk version of HTTPServer

--- src/java/higgla/server/Main.java
+++ src/java/higgla/server/Main.java
@@ -1,6 +1,12 @@
 package higgla.server;
 
 import juglr.*;
+import juglr.net.HTTP;
+import juglr.net.HTTPServer;
+
+import java.io.IOException;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * Main class for the Higgla application
@@ -11,8 +17,17 @@ import juglr.*;
 public class Main {
 
     public static void main (String[] args) {
-        // Make sure the default message bus is HTTPMessageBus
-        System.setProperty("juglr.busclass", "juglr.net.HTTPMessageBus");
+        int port = 4567;
+        if (args.length >= 1) {
+            try {
+                port = parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println(String.format(
+                        "Port not a number '%s': %s. Bailing out.",
+                        args[0], e.getMessage()));
+                System.exit(1);
+            }
+        }
 
         // Set up all central actors
         Actor store = new StoreActor();
@@ -33,6 +48,33 @@ public class Main {
 
         // Start actors
         MessageBus.getDefault().start(store.getAddress());
+        MessageBus.getDefault().start(query.getAddress());
+        MessageBus.getDefault().start(get.getAddress());
+
+        // Set up the HTTP server
+        HTTPServer server = null;
+        try {
+             server = new HTTPServer(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to create HTTP server. Bailing out.");
+            System.exit(2);
+        }
+
+        server.registerHandler(
+                    "^/[^/]+/query/?$", query.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/search\\?.+$", search.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/changes\\?.+$", changes.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/count/?$", count.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/changes\\?.+$", changes.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/[^/]+$", get.getAddress(), HTTP.Method.GET);
+        //server.registerHandler(
+        //            "^/[^/]+/[^/]+$", put.getAddress(), HTTP.Method.PUT);
 
         // Indefinite non-busy block
         synchronized (store) {
---
+++ src/java/higgla/server/PutGatewayActor.java
@@ -0,0 +1,12 @@
+package higgla.server;
+
+import juglr.Actor;
+
+/**
+ *
+ *
+ * @author Mikkel Kamstrup Erlandsen <mailto:mke@statsbiblioteket.dk>
+ * @since Feb 16, 2010
+ */
+public class PutGatewayActor extends Actor {
+}
