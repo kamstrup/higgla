098cccb2a8879bbb251c8be4ec1fb1fcecb2c4d6
Mikkel Kamstrup Erlandsen <mke@statsbiblioteket.dk>
2010-02-18 14:27:47 +0100

    Bump Juglr dep. to 0.3.1

--- higgla.sh
+++ higgla.sh
@@ -1,1 +1,1 @@
-exec java -ea -Xbootclasspath/p:lib/jsr166.jar -cp lib/lucene-core-3.0.0.jar:lib/juglr-0.3.0.jar:higgla-0.0.2.jar higgla.server.Main "$@"
+exec java -ea -Xbootclasspath/p:lib/jsr166.jar -cp lib/lucene-core-3.0.0.jar:lib/juglr-0.3.1.jar:higgla-0.0.2.jar higgla.server.Main "$@"
